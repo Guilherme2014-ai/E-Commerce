2b5f887920dd67aa61be17fd5f10b09cc7d47311
Guilherme Henrique <guilhermehenrique78383@gmail.com>
2021-07-25 17:03:40 -0300

    Header Completed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
