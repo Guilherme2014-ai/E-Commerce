012764ad4875b1fca06a51284a67107ff097cdd1
Guilherme Henrique <guilhermehenrique78383@gmail.com>
2021-07-25 21:57:26 -0300

    Nav Bug Solved, Route 'user' added

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,8 +1,26 @@
+const knex = require('../database/connection');
+
 class Admins{
 
-    Index(req,res){
-        res.render('admin/index')
+    async Index(req,res){
+        try{
+
+            res.render('admin/index')
+
+        } catch(err){
+            console.error(err)
+        }
     };
+    async Users(req,res){
+        try{
+            const users = await knex.select('*').table('users');
+            
+            res.render('admin/users', { users })
+
+        } catch(err){
+            console.error(err)
+        }
+    }
 
 };
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,6 +3,7 @@ const publicController = require('../controllers/admin');
 
 
 router.get('/', publicController.Index);
+router.get('/users', publicController.Users);
 
 
 module.exports = router;
