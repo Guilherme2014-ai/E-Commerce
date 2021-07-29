b8d46e1d383b6831bd9335b4de312d1cab361e96
Guilherme Henrique <guilhermehenrique78383@gmail.com>
2021-07-29 13:26:51 -0300

    Bug Responsivity Solved !

--- src/models/orders.js
+++ src/models/orders.js
@@ -5,14 +5,13 @@ class Users{
     async Create(order){
         try{
 
-            console.log(order)
             await knex.insert(order).table('orders');
 
         } catch(err){
             console.error(err);
             return 500;
         }
-    }
+    };
 
 };
 
--- src/routes/public.js
+++ src/routes/public.js
@@ -2,14 +2,16 @@ const router = require('express').Router();
 const publicController = require('../controllers/public');
 
 
-router.get('/', publicController.Index);
-router.get('/category/:slug', publicController.Category);
-router.get('/inventory/:id', publicController.Inventory);
-router.get('/cadastro', publicController.newAccount);
-router.post('/cadastro', publicController.newAccount_POST);
-router.get('/login', publicController.Login);
-router.post('/login', publicController.Login_POST);
-router.post('/buy', publicController.Buy); // Terminar dps que Terminar a session de login
+// Get
+    router.get('/', publicController.Index);
+    router.get('/category/:slug', publicController.Category);
+    router.get('/inventory/:id', publicController.Inventory);
+    router.get('/cadastro', publicController.newAccount);
+    router.get('/login', publicController.Login);
+// Post
+    router.post('/cadastro', publicController.newAccount_POST);
+    router.post('/login', publicController.Login_POST);
+    router.post('/buy', publicController.Buy);
 
-
+// Others
 module.exports = router;
