8f0d660ca62f092f761bc882f478c3846cd3a379
Guilherme Henrique <guilhermehenrique78383@gmail.com>
2021-08-01 15:44:13 -0300

    Checkpoint

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const path = require('path');
 const auth = require('./middlewares/auth');
 
-
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
